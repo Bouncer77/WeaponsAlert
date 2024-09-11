c6e72982e041f4ebc674072a51cc638979539962
Kosenkov <i.kosenkov77@yandex.ru>
2024-09-12 01:17:29 +0300

    Добавил настройки для Gradle

---
+++ src/main/java/pro/kosenkov/weapons/alert/WeaponsAlertApplication.java
@@ -0,0 +1,13 @@
+package pro.kosenkov.weapons.alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaponsAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeaponsAlertApplication.class, args);
+    }
+
+}
